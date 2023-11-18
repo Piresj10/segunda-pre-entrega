ca5e521055094a4f24db17e876a96ff506930eec
Javier Pires <javierpires30@gmail.com>
2023-11-18 14:42:31 -0300

    guardamos el archivo main.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='clientes',
+    version='1.0',
+    description='Segunda pre-entrega',
+    author='Javier Pires',
+    author_email='javierpires30@gmail.com',
+    packages=['clientes']      
+)
